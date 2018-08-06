4709b629f0948902ac49269ffcd1ff7ca3758123
Sara <sakie90@yahoo.com>
2018-08-07 00:04:33 +0200

    dodat slider i dropdown linkovi

--- js/main.js
+++ js/main.js
@@ -21,7 +21,83 @@ $(document).ready(function(){
         animation();
     });
     
-    
+    //SLIDER
+
+    if ($('.owl-carousel').length > 0) {
+        
+        $('.lead-slider').owlCarousel({
+            loop: true,
+            margin: 0,
+            responsiveClass: true,
+            dots: true,
+            autoplay: true,
+            nav:false,
+            items:1
+
+            
+        });
+        
+//        $('.testimonial-slider').owlCarousel({
+//            loop: true,
+//            margin: 0,
+//            responsiveClass: true,
+//            dots: true,
+//            autoplay: true,
+//            nav:false,
+//            items:1
+//
+//            
+//        });
+//        
+//        
+//        
+//        $('.team-slider').owlCarousel({
+//            loop: true,
+//            margin: 30,
+//            responsiveClass: true,
+//            dots: true,
+//            autoplay: true,
+//
+//            responsive: {
+//                0: {
+//                    items: 1
+//                },
+//                576: {
+//                    items: 2
+//                },
+//                992: {
+//                    items: 3
+//                },
+//                1200: {
+//                    items: 4
+//                }
+//            }
+//        });
+//        
+//        $('.portfolio-slider').owlCarousel({
+//            loop: true,
+//            margin: 30,
+//            responsiveClass: true,
+//            dots: true,
+//            autoplay: true,
+//
+//            responsive: {
+//                0: {
+//                    items: 1
+//                },
+//                576: {
+//                    items: 2
+//                },
+//                992: {
+//                    items: 3
+//                },
+//                1200: {
+//                    items: 4
+//                }
+//            }
+//        });
+    }
+
     
 });
 
