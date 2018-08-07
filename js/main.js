fc6655e64ae9a96360d0c553ef1114ba2d78353d
Sara Stevic <sara_stevic@yahoo.com>
2018-08-07 15:58:35 +0200

    slider i boxes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     function animation() {
 
         var windowHeight = $(window).height();
@@ -14,91 +14,33 @@ $(document).ready(function(){
             }
         });
     }
-    
+
     animation();
-    
-    $(window).scroll(function(){
+
+    $(window).scroll(function () {
         animation();
     });
-    
+
     //SLIDER
 
     if ($('.owl-carousel').length > 0) {
-        
+
         $('.lead-slider').owlCarousel({
             loop: true,
             margin: 0,
             responsiveClass: true,
             dots: true,
             autoplay: true,
-            nav:false,
-            items:1
+            nav: false,
+            items: 1
 
-            
+
         });
-        
-//        $('.testimonial-slider').owlCarousel({
-//            loop: true,
-//            margin: 0,
-//            responsiveClass: true,
-//            dots: true,
-//            autoplay: true,
-//            nav:false,
-//            items:1
-//
-//            
-//        });
-//        
-//        
-//        
-//        $('.team-slider').owlCarousel({
-//            loop: true,
-//            margin: 30,
-//            responsiveClass: true,
-//            dots: true,
-//            autoplay: true,
-//
-//            responsive: {
-//                0: {
-//                    items: 1
-//                },
-//                576: {
-//                    items: 2
-//                },
-//                992: {
-//                    items: 3
-//                },
-//                1200: {
-//                    items: 4
-//                }
-//            }
-//        });
-//        
-//        $('.portfolio-slider').owlCarousel({
-//            loop: true,
-//            margin: 30,
-//            responsiveClass: true,
-//            dots: true,
-//            autoplay: true,
-//
-//            responsive: {
-//                0: {
-//                    items: 1
-//                },
-//                576: {
-//                    items: 2
-//                },
-//                992: {
-//                    items: 3
-//                },
-//                1200: {
-//                    items: 4
-//                }
-//            }
-//        });
+
+
     }
 
-    
+
 });
 
 
